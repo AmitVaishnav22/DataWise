ff970bddc7c100eb68cb331f18926f5522cea212
AmitVaishnav22 <vaishnanamit@gmail.com>
2025-07-12 17:51:18 +0530

    Refactor app initialization and configure port from environment variable

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,11 @@ from flask import Flask
 from routes.dataset_routes import bp as dataset_bp
 from routes.quality_log_routes import quality_bp
 from flasgger import Swagger
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
 
 app = Flask(__name__)
 Swagger(app)
@@ -13,4 +18,5 @@ app.register_blueprint(quality_bp)
 def sample_route():
     return {"message": "API Health Check Successful!"}, 200
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
